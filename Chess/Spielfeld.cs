8b262200d8ab6dfb6ca0d422eef92a8d8cad63ba
Student <foobar@sr.htlweiz.at>
2026-04-09 15:44:23 +0200

    requirments and ideas collection

---
+++ Chess/Spielfeld.cs
@@ -0,0 +1,22 @@
+namespace Chess;
+
+public class Spielfeld
+{
+    //sth sth
+    /*
+    Feld array erstellen: zweidimesional, 8x8 Felder
+    x- und y-Positonen: 
+    */
+}
+
+public class Feld
+{
+    //sth sth
+    /*
+    Inhalte die ein einzelnes Feld wissen muss:
+    (Farbe: schwarz/weiß -> bool, non-nullable)
+    Status: besetzt/unbesetzt -> bool, non-nullabe (relevant für Figurenbewegung: "blockfelder")
+    Figureninhalt: welche Figur auf dem Feld steht -> extra Klasse mit Datentyp "Figur" erstellen
+    Position: überprüft, ob Feld überhaupt erstellt werden darf
+    */
+}
